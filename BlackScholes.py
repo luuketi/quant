e11eac4a0986a4aad6c4f49b29060310ef2e9787
Lucas <paradisi.lucas@gmail.com>
2019-06-12 23:38:44 -0300

    Adding comparisons

--- BlackScholes.py
+++ BlackScholes.py
@@ -2,7 +2,7 @@ from math import sqrt, pow, log, exp
 import scipy.integrate as integrate
 from numpy import pi, inf
 
-def BlackScholes( S_0, K, T, r, volatility):
+def BlackScholes( S_0, K, T, r, volatility ):
 
         d1 = 1 / (volatility * sqrt( T)) * log( S_0 * exp( r * T ) / K ) + .5 * volatility * sqrt( T )
         d2 = 1 / (volatility * sqrt(T)) * log( S_0 * exp(r * T) / K) - .5 * volatility * sqrt(T)
@@ -12,4 +12,5 @@ def BlackScholes( S_0, K, T, r, volatility):
 
         return S_0 * N(d1) - K * exp(-1*r*T) * N(d2)
 
-print(BlackScholes(S_0=60, K=62, T=0.5, r=0.06, volatility=0.13))
+if __name__ == '__main__':
+    print(BlackScholes(S_0=60, K=62, T=0.5, r=0.06, volatility=0.13))
