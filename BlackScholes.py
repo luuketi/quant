62892882b5ac6fb75db75c473c92c90ef2604187
Lucas <paradisi.lucas@gmail.com>
2019-06-21 00:40:13 -0300

    Fixing BlackScholes

--- BlackScholes.py
+++ BlackScholes.py
@@ -2,15 +2,26 @@ from math import sqrt, pow, log, exp
 import scipy.integrate as integrate
 from numpy import pi, inf
 
-def BlackScholes( S_0, K, T, r, volatility ):
+class BlackScholes( object ):
 
-        d1 = 1 / (volatility * sqrt( T)) * log( S_0 * exp( r * T ) / K ) + .5 * volatility * sqrt( T )
-        d2 = 1 / (volatility * sqrt(T)) * log( S_0 * exp(r * T) / K) - .5 * volatility * sqrt(T)
+    def __init__( self, S_0, K, T, r, volatility ):
+        self.S_0 = S_0
+        self.K = K
+        self.T = T
+        self.r = r
+        self.volatility = volatility
 
-        def N(d):
+        self.d1 = 1 / (self.volatility * sqrt(self.T)) * log( self.S_0 * exp( self.r * self.T ) / self.K ) + .5 * self.volatility * sqrt( self.T )
+        self.d2 = 1 / (self.volatility * sqrt(self.T)) * log( self.S_0 * exp( self.r * self.T ) / self.K ) - .5 * self.volatility * sqrt( self.T )
+
+    def _N( self, d ):
             return 1 / sqrt(2*pi) * integrate.quad( lambda x: exp(-1*(x**2)/2), -inf, d)[0]
 
-        return S_0 * N(d1) - K * exp(-1*r*T) * N(d2)
+    def getPrice( self ):
+        return self.S_0 * self._N( self.d1 ) - self.K * exp( -1 * self.r * self.T ) * self._N( self.d2 )
+
+    def delta( self ):
+        return self._N( self.d1 )
 
 if __name__ == '__main__':
-    print(BlackScholes(S_0=60, K=62, T=0.5, r=0.06, volatility=0.13))
+    print(BlackScholes(S_0=60, K=62, T=0.5, r=0.06, volatility=0.13).getPrice())
--- comparisons.py
+++ comparisons.py
@@ -1,5 +1,6 @@
 from BinomialTree import *
 from BlackScholes import BlackScholes
+from MonteCarlo   import MonteCarloPricer
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
@@ -11,17 +12,19 @@ def europeanCall(*args, **kwargs):
 def americanCall( *args, **kwargs ):
     return AmericanCall(*args, **kwargs).getPrice()
 
+def blackScholes( *args, **kwargs ):
+    del kwargs[ 'N' ]
+    return BlackScholes( *args, **kwargs ).getPrice()
+
 def europeanPut(*args, **kwargs):
     return EuropeanPut(*args, **kwargs).getPrice()
 
 def americanPut( *args, **kwargs ):
     return AmericanPut(*args, **kwargs).getPrice()
 
-mpl.style.use('seaborn')
-fig, axs = plt.subplots(2, 2, gridspec_kw={'hspace': 0.25, 'wspace': 0.25})
 
 callParams = { 'S_0': 60, 'K': 62, 'T': 0.5, 'r': 0.06, 'volatility': 0.13, 'N': 300 }
-putParams = { 'S_0': 64, 'K': 62, 'T': 0.5, 'r': 0.06, 'volatility': 0.13, 'N': 300 }
+putParams  = { 'S_0': 64, 'K': 62, 'T': 0.5, 'r': 0.06, 'volatility': 0.13, 'N': 300 }
 ranges = { 'S_0': range(1, 100),
            'T'  : np.arange( 0.1, 3, 0.1 ),
            'r'  : np.arange( 0, 0.5, 0.05 ),
@@ -29,19 +32,22 @@ ranges = { 'S_0': range(1, 100),
 
 
 def plot( functions, params, title):
+    mpl.style.use('seaborn')
+    fig, axs = plt.subplots(2, 2, gridspec_kw={'hspace': 0.25, 'wspace': 0.25})
     fig.suptitle('C_0 - ' + title)
 
     coord = 0
     for param, range in ranges.items():
         for i, f in enumerate(functions):
             values = [ f( **{**params, param : r } ) for r in range ]
-            axs[int(coord >1), coord % 2].plot(range, values, 'C1' if i else 'C2', label=f.__name__)
-            axs[int(coord >1), coord % 2].set(xlabel=param)
+            axs[int(coord > 1), coord % 2].plot(range, values, 'C'+str(i+1), label=f.__name__)
+            axs[int(coord > 1), coord % 2].set(xlabel=param)
             axs[int(coord > 1), coord % 2].legend()
         coord += 1
 
     plt.show()
 
 
-plot([europeanCall, americanCall], callParams, 'Call')
+plot([europeanCall, americanCall, blackScholes, MonteCarloPricer], callParams, 'Call')
 plot([europeanPut, americanPut], putParams, 'Put')
+
